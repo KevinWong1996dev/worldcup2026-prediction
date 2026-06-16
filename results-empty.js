11d8885e62c3768bfc99729e219dbd9614f0d990
KevinWong1996dev <60404173+KevinWong1996dev@users.noreply.github.com>
2026-06-16 09:01:58 -0500

    Update results-empty.js

--- results-empty.js
+++ results-empty.js
@@ -1,7 +1,73 @@
 const RESULTS = {
   groups: {},
   thirdPlace: [],
-  groupMatches: {},
+  groupMatches: {
+
+    "A": {
+      "Mexico__South Africa": { "home": 2, "away": 0 },
+      "Mexico__South Korea": { "home": 100, "away": 100 },
+      "Mexico__Czech Republic": { "home": 100, "away": 100 },
+      "South Africa__South Korea": { "home": 100, "away": 100 },
+      "South Africa__Czech Republic": { "home": 100, "away": 100 },
+      "South Korea__Czech Republic": { "home": 2, "away": 1 }
+    },
+    "B": {
+      "Canada__Switzerland": { "home": 100, "away": 100 },
+      "Canada__Qatar": { "home": 100, "away": 100 },
+      "Canada__Bosnia & Herzegovina": { "home": 1, "away": 1 },
+      "Switzerland__Qatar": { "home": 1, "away": 1 },
+      "Switzerland__Bosnia & Herzegovina": { "home": 100, "away": 100 },
+      "Qatar__Bosnia & Herzegovina": { "home": 100, "away": 100 }
+    },
+    "C": {
+      "Brazil__Haiti": { "home": 100, "away": 100 },
+      "Brazil__Morocco": { "home": 1, "away": 1 },
+      "Brazil__Scotland": { "home": 100, "away": 100 },
+      "Haiti__Morocco": { "home": 100, "away": 100 },
+      "Haiti__Scotland": { "home": 0, "away": 1 },
+      "Morocco__Scotland": { "home": 100, "away": 100 }
+    },
+    "D": {
+      "Turkey__Paraguay": { "home": 100, "away": 100 },
+      "Australia__Turkey": { "home": 2, "away": 0 },
+      "Turkey__USA": { "home": 100, "away": 100 },
+      "Paraguay__Australia": { "home": 100, "away": 100 },
+      "USA__Paraguay": { "home": 4, "away": 1 },
+      "Australia__USA": { "home": 100, "away": 100 }
+    },
+    "E": {
+      "Curaçao__Ecuador": { "home": 100, "away": 100 },
+      "Germany__Curaçao": { "home": 7, "away": 1 },
+      "Curaçao__Ivory Coast": { "home": 100, "away": 100 },
+      "Ecuador__Germany": { "home": 100, "away": 100 },
+      "Ivory Coast__Ecuador": { "home": 1, "away": 0 },
+      "Germany__Ivory Coast": { "home": 100, "away": 100 }
+    },
+    "F": {
+      "Japan__Netherlands": { "home": 2, "away": 2 },
+      "Japan__Tunisia": { "home": 100, "away": 100 },
+      "Japan__Sweden": { "home": 100, "away": 100 },
+      "Netherlands__Tunisia": { "home": 100, "away": 100 },
+      "Netherlands__Sweden": { "home": 100, "away": 100 },
+      "Tunisia__Sweden": { "home": 5, "away": 1 }
+    },
+    "G": {
+      "Egypt__Belgium": { "home": 1, "away": 1 },
+      "Egypt__Iran": { "home": 100, "away": 100 },
+      "Egypt__New Zealand": { "home": 100, "away": 100 },
+      "Belgium__Iran": { "home": 100, "away": 100 },
+      "Belgium__New Zealand": { "home": 100, "away": 100 },
+      "Iran__New Zealand": { "home": 2, "away": 2 }
+    },
+    "H": {
+      "Uruguay__Spain": { "home": 100, "away": 100 },
+      "Uruguay__Saudi Arabia": { "home": 1, "away": 1 },
+      "Uruguay__Cape Verde": { "home": 100, "away": 100 },
+      "Spain__Saudi Arabia": { "home": 100, "away": 100 },
+      "Spain__Cape Verde": { "home": 0, "away": 0 },
+      "Saudi Arabia__Cape Verde": { "home": 0, "away": 100 }
+    }
+  },
 
   knockout: {
     round32: [],
